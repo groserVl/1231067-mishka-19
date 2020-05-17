2593db81fff3c194efd15da50382d512406e7931
groserVl <59569597+groserVl@users.noreply.github.com>
2020-05-17 20:07:21 +0300

    сделаны поправки к Б13 и Б28

--- source/js/main.js
+++ source/js/main.js
@@ -1,57 +1,57 @@
 
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
-var butOrder = document.querySelector('.promo-baskets__order');
-var modalCart = document.querySelector('.modal-cart');
-var modalOverlay = document.querySelector('.modal-overlay');
-var cardCart = document.querySelectorAll('.card__cart');
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
+var butOrder = document.querySelector(".promo-baskets__order");
+var modalCart = document.querySelector(".modal-cart");
+var modalOverlay = document.querySelector(".modal-overlay");
+var cardCart = document.querySelectorAll(".card__cart");
 
 // Menu
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove("main-nav--nojs");
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
   } else {
-        navMain.classList.add('main-nav--closed');
-        navMain.classList.remove('main-nav--opened');
+        navMain.classList.add("main-nav--closed");
+        navMain.classList.remove("main-nav--opened");
   }
 });
 
 // Modal for baskets
 
 if (butOrder) {
-butOrder.addEventListener('click', function (evt) {
+butOrder.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modalCart.classList.add('modal-cart--show');
-  modalOverlay.classList.add('modal-overlay--show');
+  modalCart.classList.add("modal-cart--show");
+  modalOverlay.classList.add("modal-overlay--show");
 });
 }
 
 if (cardCart) {
 for( var i = 0; i < cardCart.length; i++)
-  cardCart[i].addEventListener('click', function (evt) {
+  cardCart[i].addEventListener("click", function (evt) {
     evt.preventDefault();
-    modalCart.classList.add('modal-cart--show');
-    modalOverlay.classList.add('modal-overlay--show');
+    modalCart.classList.add("modal-cart--show");
+    modalOverlay.classList.add("modal-overlay--show");
 });
 }
 
 if (modalOverlay) {
-modalOverlay.addEventListener('click', function(evt) {
+modalOverlay.addEventListener("click", function(evt) {
   evt.preventDefault();
-  modalCart.classList.remove('modal-cart--show');
-  modalOverlay.classList.remove('modal-overlay--show');
+  modalCart.classList.remove("modal-cart--show");
+  modalOverlay.classList.remove("modal-overlay--show");
 });
 }
 
-window.addEventListener('keydown', function(evt) {
+window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27) {
-    if (modalCart.classList.contains('modal-cart--show')) {
+    if (modalCart.classList.contains("modal-cart--show")) {
       evt.preventDefault();
-      modalCart.classList.remove('modal-cart--show');
-      modalOverlay.classList.remove('modal-overlay--show');
+      modalCart.classList.remove("modal-cart--show");
+      modalOverlay.classList.remove("modal-overlay--show");
     }
   }
 });
